de36e77259c104632901fa5d406ef19158c6c66a
jaderabbit <jabbott@retrorabbit.co.za>
2014-07-10 16:50:03 +0200

    Test for resume, with reply resume

--- vxaat/tests/test_ussd.py
+++ vxaat/tests/test_ussd.py
@@ -46,14 +46,37 @@ class TestAatUssdTransport(VumiTestCase):
         for field, expected_value in expected_field_values.iteritems():
             self.assertEqual(msg[field], expected_value)
 
-    def assert_outbound_message(self, msg, content, callback):
-        xml = ('<request>'
-               '<headertext>%s</headertext>'
-               '<options>'
-               '<option callback="%s" command="1" display="false" order="1" />'
-               '</options>'
-               '</request>') % (content, callback)
+    def assert_outbound_message(self, msg, content, callback,
+                                continue_session=True):
+        headertext = '<headertext>%s</headertext>' % content
+
+        if continue_session:
+            options = (
+                '<options>'
+                '<option callback="%s" command="1" display="false" order="1" />'
+                '</options>'
+            ) % callback
+        else:
+            options = ""
+
+        xml = ''.join([
+            '<request>',
+            headertext,
+            options,
+            '</request>',
+        ])
+
         self.assertEqual(msg, xml)
+
+    def assert_ack(self, ack, reply):
+        self.assertEqual(ack.payload['event_type'], 'ack')
+        self.assertEqual(ack.payload['user_message_id'], reply['message_id'])
+        self.assertEqual(ack.payload['sent_message_id'], reply['message_id'])
+
+    def assert_nack(self, nack, reply, reason):
+        self.assertEqual(nack.payload['event_type'], 'nack')
+        self.assertEqual(nack.payload['user_message_id'], reply['message_id'])
+        self.assertEqual(nack.payload['nack_reason'], reason)
 
     @inlineCallbacks
     def test_inbound_begin(self):
@@ -78,3 +101,60 @@ class TestAatUssdTransport(VumiTestCase):
             reply_content,
             self.callback_url(),
         )
+
+        [ack] = yield self.tx_helper.wait_for_dispatched_events(1)
+        self.assert_ack(ack, reply)
+
+    @inlineCallbacks
+    def test_inbound_resume_and_reply_with_end(self):
+
+        user_content = "I didn't expect a kind of Spanish Inquisition!"
+        d = self.tx_helper.mk_request('', request=user_content)
+        [msg] = yield self.tx_helper.wait_for_dispatched_inbound(1)
+        self.assert_inbound_message(
+            msg,
+            session_event=TransportUserMessage.SESSION_RESUME,
+            content=user_content,
+        )
+
+        reply_content = "Nobody expects the Spanish Inquisition!"
+        reply = msg.reply(reply_content, continue_session=False)
+        self.tx_helper.dispatch_outbound(reply)
+        response = yield d
+
+        self.assert_outbound_message(
+            response.delivered_body,
+            reply_content,
+            self.callback_url(),
+            continue_session=False,
+        )
+
+        [ack] = yield self.tx_helper.wait_for_dispatched_events(1)
+        self.assert_ack(ack, reply)
+
+    @inlineCallbacks
+    def test_inbound_resume_and_reply_with_resume(self):
+
+        user_content = "Well, what is it you want?"
+        d = self.tx_helper.mk_request('some-suffix', request=user_content)
+        [msg] = yield self.tx_helper.wait_for_dispatched_inbound(1)
+        self.assert_inbound_message(
+            msg,
+            session_event=TransportUserMessage.SESSION_RESUME,
+            content=user_content,
+            )
+
+        reply_content = "We want ... a shrubbery!"
+        reply = msg.reply(reply_content, continue_session=True)
+        self.tx_helper.dispatch_outbound(reply)
+        response = yield d
+
+        self.assert_outbound_message(
+            response.delivered_body,
+            reply_content,
+            self.callback_url(),
+            continue_session=True,
+        )
+
+        [ack] = yield self.tx_helper.wait_for_dispatched_events(1)
+        self.assert_ack(ack, reply)
